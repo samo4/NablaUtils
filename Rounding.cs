a5042da834583906c14c7b2b66f9f99d5e68e6ec
Samo 4 <samo.4tuna@gmail.com>
2020-05-09 16:53:01 +0200

    added double

--- Rounding.cs
+++ Rounding.cs
@@ -23,5 +23,23 @@ namespace NablaUtils
             decimal multiplier = Convert.ToDecimal(Math.Pow(10, Convert.ToDouble(places)));
             return Math.Ceiling(input * multiplier) / multiplier;
         }
+
+        public static double? RoundUp(this double? input, int places)
+        {
+            if (!input.HasValue)
+            {
+                return null;
+            }
+            else
+            {
+                return RoundUp(input.Value, places);
+            }
+        }
+
+        public static double RoundUp(this double input, int places)
+        {
+            double multiplier = Math.Pow(10, Convert.ToDouble(places));
+            return Math.Ceiling(input * multiplier) / multiplier;
+        }
     }
 }
