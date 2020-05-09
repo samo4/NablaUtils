a1c19b377bb98b7a91607e17fbf7569a5699ea3b
Samo 4 <samo.4tuna@gmail.com>
2020-05-09 16:16:18 +0200

    adding rounding extension class

---
+++ Rounding.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NablaUtils
+{
+    public static class Rounding
+    {
+        public static decimal? RoundUp(this decimal? input, int places)
+        {
+            if (!input.HasValue)
+            {
+                return null;
+            }
+            else
+            {
+                return RoundUp(input.Value, places);
+            }
+        }
+
+        public static decimal RoundUp(this decimal input, int places)
+        {
+            decimal multiplier = Convert.ToDecimal(Math.Pow(10, Convert.ToDouble(places)));
+            return Math.Ceiling(input * multiplier) / multiplier;
+        }
+    }
+}
