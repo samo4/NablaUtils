fc62862f616dfcc3fb8f43a108804885a470f1d1
Samo <samo4@users.noreply.github.com>
2020-08-06 19:11:17 +0200

    Update AsyncHelper.cs

--- AsyncHelper.cs
+++ AsyncHelper.cs
@@ -4,7 +4,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Threading.Tasks.Dataflow;
 
-namespace IceSync.NablaUtils
+namespace NablaUtils
 {
     public static class AsyncHelper
     {
