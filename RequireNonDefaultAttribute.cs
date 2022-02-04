0fc08b0e115f38457bd872b1cd6aa123c050c106
Samo 4 <samo.4tuna@gmail.com>
2022-02-04 17:48:11 +0100

    adding

---
+++ RequireNonDefaultAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NablaUtils
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter, AllowMultiple = false)]
+    public class RequireNonDefaultAttribute : ValidationAttribute
+    {
+        public RequireNonDefaultAttribute() : base("The {0} field requires a non-default value.")
+        {
+        }
+
+        public override bool IsValid(object value)
+        {
+            if (value is null)
+                return true; //You can flip this if you want. I wanted leave the responsability of null to RequiredAttribute
+            var type = value.GetType();
+            return !Equals(value, Activator.CreateInstance(Nullable.GetUnderlyingType(type) ?? type));
+        }
+    }
+}
