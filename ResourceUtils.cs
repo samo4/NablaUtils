2bce7733fabc617ee2b9ee6f26704434e3f9235e
Samo 4 <samo.4tuna@gmail.com>
2022-08-18 14:39:33 +0200

    adding resource utils

---
+++ ResourceUtils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SummerBack.NablaUtils
+{
+    public class ResourceUtils
+    {
+        public static byte[] GetEmbeddedResourceAsBytes(string resourcePath)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            using Stream stream = assembly.GetManifestResourceStream(resourcePath);
+            using BinaryReader reader = new BinaryReader(stream);
+            long length = stream.Length;
+            return reader.ReadBytes((int)length);
+        }
+    }
+}
