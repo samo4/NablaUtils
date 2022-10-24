a89988e3173f2c275968c5ad48c7340c533e6779
Samo 4 <samo.4tuna@gmail.com>
2022-10-24 14:53:26 +0200

    pagedlist

---
+++ PagedList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SummerBack.NablaUtils
+{
+    [Serializable]
+    public class PagedList<T> : List<T>
+    {
+        public int NumberOfRecords { get; set; }
+
+        public PagedList()
+        {
+        }
+
+        public PagedList(IEnumerable<T> source) : base(source)
+        {
+        }
+
+        public new PagedList<T> GetRange(int index, int count)
+        {
+            var result = new PagedList<T>();
+            result.NumberOfRecords = NumberOfRecords;
+            if (index >= 0 && count >= 0)
+            {
+                result.AddRange(base.GetRange(index, count));   
+            }
+            return result;
+        }
+    }
+}
