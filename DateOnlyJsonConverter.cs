b48ec1d479e52b1b83078e5ac31c5d1814cc4c4e
Samo 4 <samo4@users.noreply.github.com>
2023-10-05 11:40:05 +0200

    added dateonly

---
+++ DateOnlyJsonConverter.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NablaUtils
+{
+    public class DateOnlyJsonConverter : JsonConverter<DateOnly>
+    {
+        private const string Format = "yyyy-MM-dd";
+
+        public override DateOnly ReadJson(JsonReader reader,
+            Type objectType,
+            DateOnly existingValue,
+            bool hasExistingValue,
+            JsonSerializer serializer) =>
+            string.IsNullOrWhiteSpace(((string)reader.Value)) ? DateOnly.MinValue : DateOnly.ParseExact((string)reader.Value, Format, CultureInfo.InvariantCulture);
+
+        public override void WriteJson(JsonWriter writer, DateOnly value, JsonSerializer serializer) =>
+            writer.WriteValue(value.ToString(Format, CultureInfo.InvariantCulture));
+    }
+}
