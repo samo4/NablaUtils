1d467acb0b85ffe2f448d5404b0bdbe6a7718071
Samo 4 <samo4@users.noreply.github.com>
2023-10-15 13:00:05 +0200

    DataRecordHasColumn

---
+++ DataRecordHasColumn.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NablaUtils
+{
+    public static class DataRecordHasColumn
+    {
+        public static bool HasColumn(this IDataRecord dr, string columnName)
+        {
+            for (int i = 0; i < dr.FieldCount; i++)
+            {
+                if (dr.GetName(i).Equals(columnName, StringComparison.InvariantCultureIgnoreCase))
+                    return true;
+            }
+            return false;
+        }
+    }
+}
